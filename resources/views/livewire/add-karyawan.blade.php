a9a8617a7bf859c52410448bd42192985ff32d83
rizkym4l <rizkymaulana.more@gmail.com>
2024-03-15 14:51:46 +0700

    benerin validation message

--- resources/views/livewire/add-karyawan.blade.php
+++ resources/views/livewire/add-karyawan.blade.php
@@ -9,6 +9,8 @@
         <label for="nama" class="block text-sm font-medium text-gray-700">Nama</label>
         <input type="text"  class="mt-1 p-2 block w-full border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" wire:model.live='nama' placeholder="asep"  >
       </div>
+      @error('nama') <span class="error text-red-500">{{ $message }}</span>  @enderror
+
       <div class="mb-4">
         <label for="jabatan" class="block text-sm mb-1 font-medium text-gray-700">Jabatan</label>
         <select id="countries" wire:model.live='jabatan' class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
@@ -20,6 +22,8 @@
           <option value="Adminnistrasi">Adminnistrasi</option>
         </select>
       </div>
+      @error('jabatan') <span class="error text-red-500">{{ $message }}</span>  @enderror
+
       <div class="mb-4">
         <label for="departemen" class="block mb-1 text-sm font-medium text-gray-700">Departemen</label>
         <select id="countries" wire:model.live='departement' class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
@@ -31,10 +35,14 @@
           <option value="IT">IT</option>
         </select>
       </div>
+      @error('departement') <span class="error text-red-500">{{ $message }}</span>  @enderror
+
       <div class="mb-4">
         <label for="tanggal_bergabung" class="block text-sm font-medium text-gray-700">Tanggal Bergabung</label>
         <input type="date" id="tanggal_bergabung" name="tanggal_bergabung" class="mt-1 p-2 block w-full border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" wire:model.live='tanggalBergabung' placeholder=" 1 april 2020" >
       </div>
+      @error('tanggalBergabung') <span class="error text-red-500">{{ $message }}</span>  @enderror
+
       <!-- Data Detail -->
       <div class="mb-4">
         <label class="block text-sm font-medium text-gray-700">Informasi Kontak:</label>
@@ -43,6 +51,7 @@
   Alamatmu ,  Nomor teleponmu ,  Emailmu
         </textarea>
       </div>
+
       <div class="mb-4">
         <label class="block text-sm font-medium text-gray-700">Informasi Pekerjaan:</label>
         <textarea class="mt-1 p-2 block w-full border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"  wire:model.live='informasiPekerjaan' >
@@ -57,7 +66,7 @@
   Evaluasi Kinerja: (Tahun: 20.. : 4,5/5) , Penghargaan: (Karyawan Terbaik Juni 2023,karyawan sabeb)
         </textarea>
       </div>
-      @error('dataKeluarga') <span class="error text-red-500">{{ $message }}</span> @enderror
+      @error('informasiPerforma') <span class="error text-red-500">{{ $message }}</span> @enderror
 
       
       <div class="mb-4">
@@ -66,7 +75,7 @@
   Asuransi Kesehatanmu ,Dana Pensiunmu
         </textarea>
       </div>
-      @error('dataKeluarga') <span class="error text-red-500">{{ $message }}</span> @enderror
+      @error('informasiTambahan') <span class="error text-red-500">{{ $message }}</span> @enderror
 
       <div class="mb-4">
         <label class="block text-sm font-medium text-gray-700">Data Keluarga:</label>
